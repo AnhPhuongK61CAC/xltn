76946f0fce4dff99ac1560aef20cf51372d46190
AnhPhuongK61CAC <whitewolf1705@gmail.com>
2019-06-11 11:09:16 +0700

    Update

---
+++ Xulitiengnoi/src/xulytiengnoi/Country.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xulytiengnoi;
+
+import java.io.Serializable;
+
+public class Country implements Serializable {
+    
+    protected String Ten;
+    protected String Thudo;
+    protected String Vitri;
+    protected String Dientich;
+    protected String Danso;
+    protected String Quocki;
+    
+
+    public String getTen() {
+        return Ten;
+    }
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+
+    public String getThudo() {
+        return Thudo;
+    }
+
+    public void setThudo(String Thudo) {
+        this.Thudo = Thudo;
+    }
+
+    public String getVitri() {
+        return Vitri;
+    }
+
+    public void setVitri(String Vitri) {
+        this.Vitri = Vitri;
+    }
+
+    public String getDientich() {
+        return Dientich;
+    }
+
+    public void setDientich(String Dientich) {
+        this.Dientich = Dientich;
+    }
+
+    public String getDanso() {
+        return Danso;
+    }
+
+    public void setDanso(String Danso) {
+        this.Danso = Danso;
+    }
+
+    public String getQuocki() {
+        return Quocki;
+    }
+
+    public void setQuocki(String Quocki) {
+        this.Quocki = Quocki;
+    }
+
+    
+}
+   
---
+++ Xulitiengnoi/src/xulytiengnoi/Infor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xulytiengnoi;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collector;
+
+/**
+ *
+ * @author huan
+ */
+public class Infor {
+    
+    /**
+     * file txt lưu các thông tin về đất nước
+     * @return 
+     */
+    public ArrayList docFile(){
+        File file= new File("Infor.txt");
+        ArrayList<Country> list = new ArrayList<>();
+        try(Scanner scan = new Scanner(file)) {
+            while(scan.hasNext()){
+                Country w = new Country();
+                w.setTen(scan.nextLine());
+                w.setThudo(scan.nextLine());
+                w.setVitri(scan.nextLine());
+                w.setDanso(scan.nextLine());
+                w.setDientich(scan.nextLine());
+                w.setQuocki(scan.nextLine());
+                list.add(w);
+            }
+        } catch (Exception e) {}
+        return list;
+    }
+    
+}
